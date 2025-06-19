dbd64a471d9407cb1faeffa567d5b36fa268e435
Art <artgurianov@yandex.ru>
2025-06-19 12:18:59 +0700

    prisma workaround in next.config

--- apps/mhnt/next.config.ts
+++ apps/mhnt/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+
+    return config;
+  },
 };
 
 export default nextConfig;
