385021eb298c26159dd9ec234a729274e596edd6
Art <artgurianov@yandex.ru>
2025-06-25 19:03:31 +0700

    fix

--- apps/mhnt/src/components/ActionButtons/SignOutBtn.tsx
+++ apps/mhnt/src/components/ActionButtons/SignOutBtn.tsx
@@ -2,18 +2,16 @@
 
 import { authClient } from "@/lib/auth/authClient";
 import { Button } from "@shared/ui/components/button";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export const SignOutBtn = () => {
-  const router = useRouter();
-
   return (
     <Button
       onClick={() =>
         authClient.signOut({
           fetchOptions: {
             onSuccess: () => {
-              router.push("/signin"); // redirect to signin page
+              redirect("/signin"); // redirect to signin page
             },
           },
         })
