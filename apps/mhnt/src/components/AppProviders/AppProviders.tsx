6d195f786d193aea5460c0bc2fe0f4c758f3957c
Art <artgurianov@yandex.ru>
2025-06-28 21:18:37 +0700

    fix with suspense

--- apps/mhnt/src/components/AppProviders/AppProviders.tsx
+++ apps/mhnt/src/components/AppProviders/AppProviders.tsx
@@ -2,14 +2,22 @@
 
 import { useToastParam } from "@/lib/hooks/useToastParam";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode, useState } from "react";
+import { ReactNode, Suspense, useState } from "react";
+
+const ToastFromParams = () => {
+  useToastParam();
+  return null;
+};
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
-  useToastParam();
-
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <Suspense>
+        <ToastFromParams />
+      </Suspense>
+    </>
   );
 };
