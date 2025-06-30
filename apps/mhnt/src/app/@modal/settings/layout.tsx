0cddd32fc0add09cd9988d5d6fe529542ced0282
Art <artgurianov@yandex.ru>
2025-06-30 16:07:16 +0700

    fix

--- apps/mhnt/src/app/@modal/settings/layout.tsx
+++ apps/mhnt/src/app/@modal/settings/layout.tsx
@@ -1,9 +1,43 @@
 "use client";
 
-import { ReactNode } from "react";
+import { LoaderCircle } from "lucide-react";
+import {
+  ReactNode,
+  Suspense,
+  unstable_ViewTransition as ViewTransition,
+} from "react";
 
-export default function Layout({
+export default function TransitionLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
-  return <div className="h-full w-full px-2">{children}</div>;
+  return (
+    <ViewTransition
+      onUpdate={(instance) => {
+        instance.old.animate(
+          {
+            transform: ["translateX(0)", "translateX(-100%)"],
+            opacity: [1, 0],
+          },
+          { duration: 300 }
+        );
+        instance.new.animate(
+          {
+            transform: ["translateX(100%)", "translateX(0)"],
+            opacity: [0, 1],
+          },
+          { duration: 300 }
+        );
+      }}
+    >
+      <Suspense
+        fallback={
+          <div className="h-full w-full px-2 flex justify-center items-center">
+            <LoaderCircle className="animate-spin h-12 w-12" />
+          </div>
+        }
+      >
+        <div className="h-full w-full px-2">{children}</div>
+      </Suspense>
+    </ViewTransition>
+  );
 }
