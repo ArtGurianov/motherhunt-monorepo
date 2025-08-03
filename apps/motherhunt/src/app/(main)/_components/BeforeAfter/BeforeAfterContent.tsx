e7d37f237213251981414727a8ea9440c5513dd0
Art <artgurianov@yandex.ru>
2025-08-03 18:20:21 +0700

    before/after component style changed

--- apps/motherhunt/src/app/(main)/_components/BeforeAfter/BeforeAfterContent.tsx
+++ apps/motherhunt/src/app/(main)/_components/BeforeAfter/BeforeAfterContent.tsx
@@ -48,8 +48,8 @@ export const BeforeAfterContent = ({
   return (
     <div className="flex flex-col w-full justify-center items-center">
       <div className="relative flex w-full justify-center items-center py-8">
-        <div className="absolute -z-10 top-0 left-0 w-full h-full bg-gradient-to-b from-amber-900 to-yellow-900">
-          <div className="w-full h-full bg-gradient-to-t from-amber-700 to-yellow-800 steampunk" />
+        <div className="absolute -z-10 top-0 left-0 w-full h-full bg-gradient-to-b to-zinc-600 from-stone-500">
+          <div className="w-full h-full bg-gradient-to-t to-zinc-500 from-stone-400 steampunk" />
         </div>
         <div className="shrink-0">
           <Image
@@ -58,16 +58,16 @@ export const BeforeAfterContent = ({
             width="0"
             height="0"
             sizes="100vh"
-            className="h-48 lg:h-64 w-auto shadow-yellow-900 shadow-lg"
+            className="h-48 lg:h-64 w-auto shadow-zinc-700 shadow-lg"
             priority
           />
         </div>
         <div className="p-4 md:px-8 flex flex-col gap-4 justify-between self-stretch">
-          <h2 className="text-3xl font-light lg:text-7xl w-full text-amber-950 text-end font-steampunk-title">
-            {"HOW THE INDUSTRY WAS BACK THEN"}
+          <h2 className="text-3xl font-light lg:text-7xl w-full text-zinc-700 text-end font-steampunk-title">
+            {"HOW WAS INDUSTRY BACK THEN"}
           </h2>
           <div className="grow flex justify-center items-center">
-            <span className="text-md lg:text-2xl text-end italic font-dyna-puff font-thin text-yellow-950">
+            <span className="text-md lg:text-2xl text-end italic font-dyna-puff font-thin text-stone-700">
               {
                 "'So there was a brave scouter working for that one little agency. Used to have a few coins thrown at him from time to time… That morning agency booker came in bad mood - the real GEM newface model got skipped and lost forever…'"
               }
@@ -76,10 +76,10 @@ export const BeforeAfterContent = ({
         </div>
       </div>
       <div ref={afterContainerRef} className="relative flex flex-col w-full">
-        <div className="absolute -z-10 top-0 left-0 w-full h-full bg-gradient-to-b from-yellow-900 to-amber-900">
-          <div className="w-full h-full bg-gradient-to-t from-yellow-800 to-amber-700 steampunk" />
+        <div className="absolute -z-10 top-0 left-0 w-full h-full bg-gradient-to-b from-zinc-600 to-stone-500">
+          <div className="w-full h-full bg-gradient-to-t from-zinc-500 to-stone-400 steampunk" />
         </div>
-        <h2 className="text-5xl font-light lg:text-7xl w-full text-secondary text-center font-steampunk-title mt-4">
+        <h2 className="text-5xl font-light lg:text-7xl w-full text-secondary text-center font-steampunk-title mt-4 text-nowrap">
           {"! BUT NOT ANYMORE !"}
         </h2>
         <div className=" w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 place-items-center">
@@ -94,7 +94,7 @@ export const BeforeAfterContent = ({
                 className="h-full w-auto opacity-80 animate-pulse"
                 priority
               />
-              <span className="absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2 text-center text-4xl font-bold font-mono text-black/60 rotate-6">
+              <span className="absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2 text-center text-4xl font-bold font-mono text-zinc-800/80 rotate-6">
                 {each.label}
               </span>
             </div>
