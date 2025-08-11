63e696fd96153a393abbc63da70cb2b57ad0d76f
Art <artgurianov@yandex.ru>
2025-08-11 14:00:31 +0700

    fix

--- apps/mhnt/src/lib/web3/constants.ts
+++ apps/mhnt/src/lib/web3/constants.ts
@@ -1,4 +1,4 @@
 import { stringToHex } from "viem";
 
-export const ZERO_ADDRESS = stringToHex("", { size: 42 });
+export const ZERO_ADDRESS = stringToHex("", { size: 20 });
 export const ZERO_BYTES = stringToHex("", { size: 32 });
