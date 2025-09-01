ccc5aebe85735cf74cce796f48aec8dc7055c69f
Art <artgurianov@yandex.ru>
2025-09-01 18:11:30 +0700

    router push fix

--- apps/mhnt/src/components/Guards/SignedOutGuard.tsx
+++ apps/mhnt/src/components/Guards/SignedOutGuard.tsx
@@ -1,28 +1,42 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { authClient } from "@/lib/auth/authClient";
 import { useRouter } from "next/navigation";
 import { generateUpdatedPathString } from "@/lib/utils/generateUpdatedPathString";
 import { APP_ROUTES, APP_ROUTES_CONFIG } from "@/lib/routes/routes";
+import { StatusCard, StatusCardTypes } from "@shared/ui/components/StatusCard";
 
 interface SignedOutGuardProps {
   children: ReactNode;
 }
 
 export const SignedOutGuard = ({ children }: SignedOutGuardProps) => {
-  const { data: session } = authClient.useSession();
+  const { data: session, isPending: isSessionPending } =
+    authClient.useSession();
 
   const router = useRouter();
 
-  if (session) {
-    router.push(
-      generateUpdatedPathString(
-        APP_ROUTES_CONFIG[APP_ROUTES.AUCTION].href,
-        new URLSearchParams({
-          toast: "SIGNED_IN",
-        })
-      )
+  useEffect(() => {
+    if (session) {
+      router.push(
+        generateUpdatedPathString(
+          APP_ROUTES_CONFIG[APP_ROUTES.AUCTION].href,
+          new URLSearchParams({
+            toast: "SIGNED_IN",
+          })
+        )
+      );
+    }
+  }, [session]);
+
+  if (isSessionPending || session) {
+    return (
+      <StatusCard
+        type={StatusCardTypes.LOADING}
+        title={"Loading..."}
+        className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
+      />
     );
   }
 
