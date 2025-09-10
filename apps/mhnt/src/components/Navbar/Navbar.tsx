1eb14f1ded5590c601d301a7f20a1cd6d1ade94f
Art <artgurianov@yandex.ru>
2025-09-10 13:16:08 +0700

    Navbar animation improved

--- apps/mhnt/src/components/Navbar/Navbar.tsx
+++ apps/mhnt/src/components/Navbar/Navbar.tsx
@@ -5,16 +5,24 @@ import { authClient } from "@/lib/auth/authClient";
 import { Button } from "@shared/ui/components/button";
 import { ChevronsLeft, MenuIcon, XIcon } from "lucide-react";
 import { cn } from "@shared/ui/lib/utils";
-import { useState, useMemo, useCallback } from "react";
+import { useState, useMemo, useCallback, useEffect } from "react";
 import { AppRole } from "@/lib/auth/permissions/app-permissions";
 import { CustomMemberRole, getCustomMemberRole } from "@/lib/auth/customRoles";
 import { APP_ROUTES, APP_ROUTES_CONFIG } from "@/lib/routes/routes";
 import { OrgType } from "@/lib/utils/types";
 import { OrgRole } from "@/lib/auth/permissions/org-permissions";
 import { NavbarContent } from "./NavbarContent";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const Navbar = () => {
-  const { data: session } = authClient.useSession();
+  const [isInitialized, setIsInitialized] = useState(false);
+  const { data: session, isPending: isSessionPending } =
+    authClient.useSession();
+
+  useEffect(() => {
+    if (isInitialized || isSessionPending) return;
+    setIsInitialized(true);
+  }, [isSessionPending]);
 
   const activeRole: AppRole | CustomMemberRole | null = useMemo(() => {
     if (!session) return null;
@@ -46,73 +54,90 @@ export const Navbar = () => {
     setIsMenuOpened((prev) => !prev);
   }, []);
 
-  if (pathname.startsWith(APP_ROUTES_CONFIG[APP_ROUTES.SIGN_IN].href))
+  if (
+    pathname.startsWith(APP_ROUTES_CONFIG[APP_ROUTES.SIGN_IN].href) ||
+    !isInitialized
+  )
     return null;
 
   return (
-    <nav
-      className={cn(
-        "h-nav fixed bottom-4 left-1/2 -translate-x-1/2 lg:left-4 lg:translate-x-0 transition-all duration-700 ease-in-out",
-        {
-          "-translate-x-full left-0 lg:-translate-x-full lg:left-0":
-            isNavbarCollapsed,
-        }
-      )}
-    >
-      <div className="relative h-full">
-        <div
-          className={cn(
-            "h-full flex border rounded-2xl overflow-clip opacity-100 transition-all duration-700 ease-in-out",
-            {
-              "opacity-0": isNavbarCollapsed,
-            }
-          )}
-        >
+    <AnimatePresence>
+      <motion.nav
+        key={activeRole}
+        initial={{ transform: "translateY(100%)", opacity: 0 }}
+        animate={{
+          transform: "translateY(0)",
+          opacity: 1,
+        }}
+        exit={{ transform: "translateY(100%)", opacity: 0 }}
+        transition={{
+          ease: "easeInOut",
+          duration: 0.5,
+          delay: 0,
+        }}
+        className={cn(
+          "h-nav fixed bottom-4 left-1/2 -translate-x-1/2 lg:left-4 lg:translate-x-0 transition-all duration-700 ease-in-out",
+          {
+            "-translate-x-full left-0 lg:-translate-x-full lg:left-0":
+              isNavbarCollapsed,
+          }
+        )}
+      >
+        <div className="relative h-full">
           <div
-            className={
-              "relative flex h-full justify-center items-center bg-main/95"
-            }
+            className={cn(
+              "h-full flex border rounded-2xl overflow-clip opacity-100 transition-all duration-700 ease-in-out",
+              {
+                "opacity-0": isNavbarCollapsed,
+              }
+            )}
           >
-            <>
-              <NavbarContent
-                activeRole={activeRole}
-                isMenuOpened={isMenuOpened}
-              />
-              {session ? (
-                <div className="relative w-12 h-full md:hidden">
-                  <div className="absolute h-full w-full top-0 left-0 bg-secondary/95 flex justify-center items-center border-l">
-                    <Button
-                      size="reset"
-                      variant="ghost"
-                      className="md:hidden [&_svg]:pointer-events-auto [&_svg]:size-8"
-                      onClick={toggleMenuOpened}
-                    >
-                      {isMenuOpened ? <XIcon /> : <MenuIcon />}
-                    </Button>
+            <div
+              className={
+                "relative flex h-full justify-center items-center bg-main/95"
+              }
+            >
+              <>
+                <NavbarContent
+                  activeRole={activeRole}
+                  isMenuOpened={isMenuOpened}
+                />
+                {session ? (
+                  <div className="relative w-12 h-full md:hidden">
+                    <div className="absolute h-full w-full top-0 left-0 bg-secondary/95 flex justify-center items-center border-l">
+                      <Button
+                        size="reset"
+                        variant="ghost"
+                        className="md:hidden [&_svg]:pointer-events-auto [&_svg]:size-8"
+                        onClick={toggleMenuOpened}
+                      >
+                        {isMenuOpened ? <XIcon /> : <MenuIcon />}
+                      </Button>
+                    </div>
                   </div>
-                </div>
-              ) : null}
-            </>
+                ) : null}
+              </>
+            </div>
           </div>
+          <Button
+            size="reset"
+            variant="ghost"
+            className={cn(
+              "absolute top-1/2 -translate-y-1/2 -right-px translate-x-full [&_svg]:pointer-events-auto [&_svg]:size-10",
+              {
+                "-right-4": isNavbarCollapsed,
+              }
+            )}
+            onClick={toggleNavbarCollapsed}
+          >
+            <ChevronsLeft
+              className={cn("", {
+                "rotate-180": isNavbarCollapsed,
+              })}
+            />
+          </Button>
         </div>
-        <Button
-          size="reset"
-          variant="ghost"
-          className={cn(
-            "absolute top-1/2 -translate-y-1/2 -right-px translate-x-full [&_svg]:pointer-events-auto [&_svg]:size-10",
-            {
-              "-right-4": isNavbarCollapsed,
-            }
-          )}
-          onClick={toggleNavbarCollapsed}
-        >
-          <ChevronsLeft
-            className={cn("", {
-              "rotate-180": isNavbarCollapsed,
-            })}
-          />
-        </Button>
-      </div>
-    </nav>
+      </motion.nav>
+    </AnimatePresence>
   );
 };
